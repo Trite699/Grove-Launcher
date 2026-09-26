1e6f07f9d072bf41bae135cd3ee613062ccbf9c6
Phoenix Trite <161424109+Trite699@users.noreply.github.com>
2026-09-26 10:36:55 +0530

    Change window title from 'Attorney Online' to 'Mystical Grove'

--- launcher/mainwindow.cpp
+++ launcher/mainwindow.cpp
@@ -32,7 +32,7 @@ MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
-    setWindowTitle(tr("Attorney Online Launcher %1")
+    setWindowTitle(tr("Mystical Grove Launcher %1")
                    .arg(QCoreApplication::applicationVersion()));
 }
 
